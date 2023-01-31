62b0d56fc591728e50648e8d39375b1ad2d2db1b
SeifEleslam <seifgouda98@gmail.com>
2023-01-31 19:05:14 +0200

    missed changes by accident

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from "react";
 import { feedbacks } from "../App";
 
 export const Statistics = (props) => {
-  const [total, setTotal] = useState(0);
-  const [rate, setRate] = useState(0);
-  useEffect(() => {
-    console.log(props.feedback);
+  const total = (function () {
     let tempTotal = 0;
-    let tempRate = 0;
     Object.keys(props.feedback).forEach((key) => {
       tempTotal += props.feedback[key];
+    });
+    return tempTotal;
+  })();
+
+  const rate = (function () {
+    let tempRate = 0;
+    Object.keys(props.feedback).forEach((key) => {
       if (key === "good") tempRate += props.feedback[key];
       else if (key === "bad") tempRate -= props.feedback[key];
     });
-    setTotal(tempTotal);
-    setRate(tempRate);
-  }, [props.feedback]);
+    return tempRate;
+  })();
+
   return (
     <>
       <h1>Statistics</h1>
